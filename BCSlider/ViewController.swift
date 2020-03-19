678b911ad14f027414ec59e62d7319eee87b6a35
danielVebman <danveb800@gmail.com>
2020-03-19 12:52:10 -0400

    Initial Commit

---
+++ BCSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BCSlider
+//
+//  Created by Daniel Vebman on 3/19/20.
+//  Copyright © 2020 Daniel Vebman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
